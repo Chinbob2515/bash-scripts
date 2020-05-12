24052b1fa85e61f6ee78d0a57a62e03caeffdac0
Ben Hack <2515hack@googlemail.com>
2020-05-12 19:15:09 +0100

    Tweaked install.bash

--- install.bash
+++ install.bash
@@ -2,12 +2,21 @@
 # a command just to place all these scripts into a default PATH location
 # requires sudo
 
-# really don't want to overwrite files if they already exist and are not versions of our files
+if [ "$EUID" -ne 0 ]; then 
+	echo "Please run as root"
+	exit 1
+fi
+
+# moves first argument to our aim
+# move <path>
+AIM="/usr/local/bin/"
+move() {
+	# really don't want to overwrite files if they already exist and are not versions of our files
+	cp -i -v $1 $AIM
+}
 alias cp="cp -i -v"
 
 
 # move the files into place
 
-AIM="/usr/local/bin/"
-
-cp daemon/daemon $AIM
+move daemon/daemon
